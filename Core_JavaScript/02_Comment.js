b98fa51e8a62f2e4523d3fb13103a5fe97166a23
Ujjwal Kumar <ujjwalkumarjietcs@gmail.com>
2024-01-12 00:07:05 +0530

    Comment

---
+++ Core_JavaScript/02_Comment.js
@@ -0,0 +1,15 @@
+console.log("Welcome to javascript");
+// single line Comment
+// My name is Ujjwal Kumar
+
+
+/*
+* This is multi line comment
+* This is multi line comment
+* This is multi line comment
+* This is multi line comment
+* This is multi line comment
+* This is multi line comment
+* This is multi line comment
+* This is multi line comment
+*/ 
