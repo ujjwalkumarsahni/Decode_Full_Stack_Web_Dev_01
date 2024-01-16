6eb3876552b70251e84cafba4e370181e6e419ff
Ujjwal Kumar <ujjwalkumarjietcs@gmail.com>
2024-01-16 23:44:02 +0530

    Conditional statement

---
+++ 06_ConditionalStatement.js
@@ -0,0 +1,26 @@
+let age = 20;
+if(age>=18){
+    console.log("you can voit!");
+}
+
+let num = 19;
+if(num%2==0){
+    console.log("Even number ");
+}
+else{
+    console.log("Odd number ");
+}
+
+let marks = 73;
+if(marks>=90){
+    console.log("Gradde A");
+}
+else if(marks>=70 && marks<90){
+    console.log("Grade B");
+}
+else{
+    console.log("Grade C");
+}
+
+
+
