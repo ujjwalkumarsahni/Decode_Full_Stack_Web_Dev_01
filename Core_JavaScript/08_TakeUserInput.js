251ada1529e24aa951131900aabe66f09a7a98c7
Ujjwal Kumar <ujjwalkumarjietcs@gmail.com>
2024-01-17 09:40:09 +0530

    ternery , switch

---
+++ Core_JavaScript/08_TakeUserInput.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Enter a number :")
+if(num1%5==0){
+    console.log("number is divisible by 5");
+}
+else{
+    console.log("number is divisible by 5");
+}
---
+++ Core_JavaScript/09_switchStatement.js
@@ -0,0 +1,17 @@
+const foo = 0;
+switch (foo) {
+  case -1:
+    console.log("negative 1");
+    break;
+  case 0:
+    console.log("ZERO");
+    break;
+  case 1:
+    console.log("ONE");
+    break;
+  case 2:
+    console.log("TWO");
+    break;
+  default:
+    console.log("default");
+}
