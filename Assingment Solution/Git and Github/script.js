9f39ca4627ccf1347ceef3c197614c4e18a68b35
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2025-02-27 00:03:05 +0530

    done

---
+++ Assingment Solution/Git and Github/script.js
@@ -0,0 +1,19 @@
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("World"));
+
+function personalizedGreeting(name, age) {
+    return `Hello, ${name}! You are ${age} years old.`;
+}
+
+console.log(personalizedGreeting("Alice", 25));
+
+
+function farewell(name) {
+    return `Goodbye, ${name}!`;
+}
+
+console.log(farewell("Alice"));
+
